efb7dda0f67215253b680f4d0dedfbc03fa9b57a
Lorin Beer <lorin.beer@nitobi.com>
2012-06-05 23:32:06 -0700

    fixed issue with art resource meta data mismatch

--- client/penderdemo.js
+++ client/penderdemo.js
@@ -1,9 +1,15 @@
 //==============================================================================
+//==============================================================================
+var buildbotmeta = {
+    borderwidth : 1
+}
+
+
 function Point(x,y) {
     this.x  = x || 0;
     this.y = y || 0;
 }
-//==============================================================================
+
 
 /**
  * storage class for animation data
@@ -35,7 +41,7 @@ function Animation(framemap, framenumb, cols, rows, framewidth, frameheight, bor
 	    for (var row  = 0; row < rows && !done; row++) {
 		for (var col = 0; col < cols && !done; col++) {
 		    top = new Point( col * (frameheight+1)+(col+1)*bordersize, row * (framewidth+1) +(row+1)* bordersize);
-		    console.log(top);
+		 
 		    this._frames.push ( top );
 		    i+=1;
 		    if( i >= framenumb ) { done = true; }
@@ -46,10 +52,12 @@ function Animation(framemap, framenumb, cols, rows, framewidth, frameheight, bor
 
 
     this.draw = function(texid, dx, dy, dWidth, dHeight) {
+
+	
 	//absolute value of current frame index is used, allowing for negative frame counts
-       	var curframe = this._currentframe<0? -1 * this._currentframe : this._currentframe;
+	var curframe = this._currentframe<0? -1 * this._currentframe : this._currentframe;
 	var frametop = this._frames[curframe];
-	//console.log( this._frames[curframe], this._framewidth, this._frameheight, dWidth, dHeight);
+	
 	Pender.canvas.drawImage(Pender.getImage(texid), frametop.x, frametop.y, this._framewidth, this._frameheight,
 			     dx, dy, dWidth, dHeight);
 
@@ -59,10 +67,7 @@ function Animation(framemap, framenumb, cols, rows, framewidth, frameheight, bor
 
 
 }
-//==============================================================================
-/**
- *
- */
+
 function AnimatedSprite(anim) {
     this.xpos = 0;
     this.ypos = 0;
@@ -76,44 +81,34 @@ function AnimatedSprite(anim) {
    this.draw = function(texid) {
 	self.xpos = self.xpos+ self.xvel;
 	self.ypos = self.ypos+ self.yvel;
-	if( self.xpos < 0 || self.xpos >= 400-self.width/*Pender.canvas.width*/) {
+	if( self.xpos < 0 || self.xpos >= 720-self.width/*Pender.canvas.width*/) {
 	    self.xvel = self.xvel * -1; 
 	}
-	if(self.ypos < 0 || self.ypos >= 400-self.height/*Pender.canvas.height */) { 
+	if(self.ypos < 0 || self.ypos >= 1084-self.height/*Pender.canvas.height */) { 
 	    self.yvel = self.yvel * -1; 
 	}
 	
-	this._animation.draw( texid, self.xpos, self.ypos, self.width, self.height );
+	self._animation.draw( texid, self.xpos, self.ypos, self.width, self.height );
     };
 }
-//==============================================================================
-/**
- *
- */
+
+
 var Bots = new function () {
-    this.numb = 12;
+    this.numb = 5;
     this.bots = [];
     this.texid = 0;
     var self = this;
     this.init = function() {
 	console.log("initing");
-        texid = Pender.loadImage("demos/client/assets/build_bot_map_med.png");
+        self.texid = Pender.loadImage ("demos/client/assets/build_bot_map.png");
 	for (var i = 0; i < self.numb; i++) {
-	    console.log("newbot");
-	    var anim = new Animation (texid, 9, 4, 3, 126, 126, 1);
+	    var anim = new Animation (self.texid, 9, 4, 3, 126, 126, 1);
 	    self.bots.push (new AnimatedSprite(anim));
 	}
     };
-
-    this.testdraw = function() { 
-	Pender.canvas.clearRect(0,0,Pender.width,Pender.height);
-	Pender.canvas.fillStyle = "rgb(0,0,0)"; 
-	Pender.canvas.fillRect (10, 10, Pender.width, Pender.height);
-	self.bots[0].draw(self.texid);
-    };
-
+    
     this.draw =  function() {
-	Pender.canvas.clearRect(0,0,Pender.width, Pender.height);
+	Pender.canvas.clearRect(0,0,400,400 );
       	for(var i = 0; i < self.numb; i++) {
 	    self.bots[i].draw(self.texid);    	     
 	}
@@ -121,7 +116,9 @@ var Bots = new function () {
     
 }
 
-function init() {
-    Bots.init();
-}
 
+function init() {
+    console.log("INIT CALLED");
+    Bots.init();
+    Pender.setInterval(Bots.draw,50);
+}
